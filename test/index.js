ac7727f3a1aa7a797b98b9a4ae2f9990051873c7
marcusnewton1 <marcus.newton@hotmail.com>
2019-01-14 17:49:16 +1100

    tests failing with container

--- test/index.js
+++ test/index.js
@@ -1,40 +1,56 @@
 // This test file uses the tape testing framework.
 // To learn more, go here: https://github.com/substack/tape
-const test = require('tape');
-const Container = require('@holochain/holochain-nodejs');
-// const { Config, Container } = require('@holochain/holochain-nodejs');
+const test = require('tape')
+const { Config, Container } = require('@holochain/holochain-nodejs')
 
 const dnaPath = "dist/bundle.json"
 
-// Multi-agent container scenario testing (waiting for container version 0.3.0)
+const aliceName = "alice"
+const tashName = "tash"
+
+const agentAlice = Config.agent(aliceName)
+const agentTash = Config.agent(tashName)
+const dna = Config.dna(dnaPath)
+const instanceAlice = Config.instance(agentAlice, dna)
+const instanceTash = Config.instance(agentTash, dna)
+const config = Config.container([instanceAlice, instanceTash])
+
+const container = new Container(config)
+
+const aliceInstanceId = aliceName + '::' + dnaPath
+
+test('create an article', (t) => {
+  t.plan(2)
+
+  const alice = container.makeCaller(aliceName, dnaPath)
+
+  const input = {
+    article: {
+      title: "Article Title",
+      abst: "abstract text",
+      body: "body of article"
+    }
+  }
+
+  const expect = {
+    "success": true
+  }
+
+  const aliceResult = alice.call("articles", "main", "create_article", input)
+  console.log(result);
+
+  t.deepEqual(aliceResult.success, expect.success)
+  t.deepEqual(aliceResult.address.length, 46)
+
+  t.end()
+})
+
+// stop all running instances
+container.stop()
+
+
+
 /*
-const multi_container = (function() {
-  const agentMarcus = Config.agent("marcus")
-  const agentCameron = Config.agent("cameron")
-
-  const dna = Config.dna(dnaPath)
-
-  const instanceMarcus = Config.instance(agentMarcus, dna)
-  const instanceCameron = Config.instance(agentCameron, dna)
-
-  const containerConfig = Config.container([instanceMarcus, instanceCameron])
-  return new Container(containerConfig)
-}());
-
-activate container instance
-multi_container.start()
-
-const marcus = multi_container.makeCaller('marcus', dnaPath)
-const cameron = multi_container.makeCaller('cameron', dnaPath)
-*/
-
-// Basic container scenario testing
-const container = Container.loadAndInstantiate(dnaPath)
-
-container.start()
-
-
-// Non-agent tests
 test('create an article', (t) => {
   t.plan(2)
   const input = {
@@ -76,16 +92,5 @@ test('get article', (t) => {
   t.deepEqual(result, expect)
 
   t.end()
-})
-
-
-// Multi-agent tests
-/*
-test('agentId', (t) => {
-  t.plan(2)
-
-  t.ok(marcus.agentId)
-
-  t.notEqual(marcus.agentId, cameron.agentId)
 })
 */
