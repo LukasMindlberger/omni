5b59998d8e37719bd703e202d1197dbaf1b80dd7
Marcus Newton <marcus.newton@hotmail.com>
2019-02-21 11:34:21 +1100

    updating tests

--- test/index.js
+++ test/index.js
@@ -1,322 +1,362 @@
 // This test file uses the tape testing framework.
 // To learn more, go here: https://github.com/substack/tape
-const test = require('tape')
-// const tapSpec = require('tap-spec');
-// test.createStream()
-//   .pipe(tapSpec())
-//   .pipe(process.stdout);
+const { Config, Scenario } = require("@holochain/holochain-nodejs");
+Scenario.setTape(require("tape"));
 
-const { Config, Conductor } = require('@holochain/holochain-nodejs')
+const dnaPath = "./dist/bundle.json";
+const agentAlice = Config.agent("alice");
+const agentCameron = Config.agent("cameron");
+const dna = Config.dna(dnaPath);
+const instanceAlice = Config.instance(agentAlice, dna);
+const instanceCameron = Config.instance(agentCameron, dna);
+const scenario = new Scenario([instanceAlice, instanceCameron]);
 
-const dnaPath = "./dist/bundle.json"
-
-const aliceName = "alice"
-const cameronName = "cameron"
-
-// closure to keep config-only stuff out of test scope
-const container = (() => {
-
-  const agentAlice = Config.agent(aliceName)
-  const agentCameron = Config.agent(cameronName)
-
-  const dna = Config.dna(dnaPath)
-
-  const instanceAlice = Config.instance(agentAlice, dna)
-  const instanceCameron = Config.instance(agentCameron, dna)
-
-  const containerConfig = Config.container([instanceAlice, instanceCameron])
-
-  return new Conductor(containerConfig)
-})()
-
-container.start()
-
-const alice = container.makeCaller(aliceName, dnaPath)
-const cameron = container.makeCaller(cameronName, dnaPath)
-
-// Tests
-test('has agentId', (t) => {
-  t.plan(2)
+scenario.runTape("has agentID", (t, { alice, cameron }) => {
+  t.plan(2);
 
   console.log(alice.agentId);
   console.log(cameron.agentId);
 
-  t.ok(alice.agentId, "alice should have id")
-  t.ok(cameron.agentId, "cameron should have id")
-})
+  t.ok(alice.agentId, "alice should have id");
+  t.ok(cameron.agentId, "cameron should have id");
+});
 
-test('show environment data', (t) => {
-  t.plan(1)
+scenario.runTape("show environment data", (t, { alice, cameron }) => {
+  t.plan(1);
 
-  const result = alice.call("articles", "main", "show_env", {})
+  const result = alice.call("articles", "show_env", {});
 
   console.log(result);
 
-  t.ok(result)
+  t.ok(result);
+});
 
-  t.end()
-})
-
-test('alice send message to cameron', (t) => {
-  t.plan(1)
+scenario.runTape("alice send message to cameron", (t, { alice, cameron }) => {
+  t.plan(1);
 
   const input = {
     to_agent: cameron.agentId,
     message: "Hi Cameron"
-  }
+  };
 
-  const response = alice.call("messages", "main", "send_message", input)
-
-  // console.log(response);
-
-  t.ok(response.success, "Message should reach destination if both agents online")
-
-  t.end()
-})
-
-test('create an article', (t) => {
-  t.plan(2)
-
-  const input = {
-    title: "Article Title",
-    abst: "abstract text",
-    body: "body of article"
-  }
-
-  const response = alice.call("articles", "main", "create_article", input)
+  const response = alice.call("messages", "send_message", input);
 
   // console.log(response);
 
-  t.ok(response.Ok, "Should be able to create article")
-  t.deepEqual(response.Ok.length, 46, "Address length should be 46 chars")
+  t.ok(
+    response.success,
+    "Message should reach destination if both agents online"
+  );
+});
 
-  t.end()
-})
+scenario.runTape(
+  "create an article and get the article",
+  (t, { alice, cameron }) => {
+    t.plan(4);
 
-test('get the article', (t) => {
-  t.plan(2)
+    // create
+    const input = {
+      title: "Article Title",
+      abst: "abstract text",
+      body: "body of article"
+    };
 
-  const input = {
-    article_addr: "QmTuvXiW6MRXG4gQsXSTPPVqxwPCp6ytDxboiLVsTSThbc"
+    const response = alice.call("articles", "create_article", input);
+
+    // console.log(response);
+
+    t.ok(response.Ok, "Should be able to create article");
+    t.deepEqual(response.Ok.length, 46, "Address length should be 46 chars");
+
+    // get
+    const input2 = {
+      article_addr: response.Ok
+    };
+
+    const response2 = alice.call("articles", "get_article", input2);
+
+    // console.log(response);
+
+    t.ok(
+      response2.Ok,
+      "hdk::get_entry should return article content at address"
+    );
+    t.deepEqual(
+      JSON.parse(response2.Ok.App[1]),
+      input,
+      "Returned article should match expected data"
+    );
   }
+);
 
-  const expect = {
-    title: "Article Title",
-    abst: "abstract text",
-    body: "body of article"
-  }
-
-  const response = alice.call("articles", "main", "get_article", input)
-
-  // console.log(response);
-
-  t.ok(response.Ok, "hdk::get_entry should return article content at address for any user")
-  t.deepEqual(JSON.parse(response.Ok.App[1]), expect, "Returned article should match expected data")
-
-  t.end()
-})
-
-test('link article to keyword', (t) => {
-  t.plan(1)
+/*
+test("link article to keyword", t => {
+  t.plan(1);
   const input = {
     keyword: {
       keyword: "Astrophysics"
     },
     article_addr: "QmTuvXiW6MRXG4gQsXSTPPVqxwPCp6ytDxboiLVsTSThbc"
-  }
+  };
 
-  const response = alice.call("articles", "main", "link_article_from_keyword", input)
+  const response = alice.call(
+    "articles",
+    "main",
+    "link_article_from_keyword",
+    input
+  );
 
   console.log(response);
 
-  t.ok(response.ok, "Shouldn't receive unimplemented error")
+  t.ok(response.ok, "Shouldn't receive unimplemented error");
 
-  t.end()
-})
+  t.end();
+});
 
-test('get article sources', (t) => {
-  t.plan(1)
+test("get article sources", t => {
+  t.plan(1);
 
   const input = {
     address: "QmTuvXiW6MRXG4gQsXSTPPVqxwPCp6ytDxboiLVsTSThbc"
-  }
+  };
 
-  const response = cameron.call("articles", "main", "get_sources_latest", input)
-
-  console.log(response);
-
-  t.ok(response.Ok, "Should receive sources for article")
-
-  t.end()
-})
-
-test('cameron get article addresses authored by alice', (t) => {
-  t.plan(2)
-
-  const input = {
-   agent_addr: alice.agentId
-  }
-
-  const response = cameron.call("articles", "main", "get_authored_articles", input)
+  const response = cameron.call(
+    "articles",
+    "main",
+    "get_sources_latest",
+    input
+  );
 
   console.log(response);
 
-  t.ok(response.Ok, "hdk::get_links shouldn't return Err")
-  t.ok(response.Ok.addresses[0] != undefined, "Should return addresses of live articles Alice has created")
+  t.ok(response.Ok, "Should receive sources for article");
 
-  t.end()
-})
+  t.end();
+});
 
-test('delete the article', (t) => {
-  t.plan(1)
+test("cameron get article addresses authored by alice", t => {
+  t.plan(2);
+
+  const input = {
+    agent_addr: alice.agentId
+  };
+
+  const response = cameron.call(
+    "articles",
+    "main",
+    "get_authored_articles",
+    input
+  );
+
+  console.log(response);
+
+  t.ok(response.Ok, "hdk::get_links shouldn't return Err");
+  t.ok(
+    response.Ok.addresses[0] != undefined,
+    "Should return addresses of live articles Alice has created"
+  );
+
+  t.end();
+});
+
+test("delete the article", t => {
+  t.plan(1);
 
   const input = {
     article_addr: "QmTuvXiW6MRXG4gQsXSTPPVqxwPCp6ytDxboiLVsTSThbc"
-  }
+  };
 
-  const response = alice.call("articles", "main", "delete_article", input)
+  const response = alice.call("articles", "main", "delete_article", input);
 
   // console.log(response);
 
-  t.ok(response.Ok === null, "Alice should be able to delete her article")
+  t.ok(response.Ok === null, "Alice should be able to delete her article");
 
-  t.end()
-})
+  t.end();
+});
 
-test('fail to get deleted article', (t) => {
-  t.plan(1)
+test("fail to get deleted article", t => {
+  t.plan(1);
 
   const input = {
     article_addr: "QmTuvXiW6MRXG4gQsXSTPPVqxwPCp6ytDxboiLVsTSThbc"
-  }
+  };
 
-  const response = alice.call("articles", "main", "get_article", input)
+  const response = alice.call("articles", "main", "get_article", input);
 
   // console.log(response);
 
-  t.ok(response.Ok === null, "Deletion of entry should return null")
+  t.ok(response.Ok === null, "Deletion of entry should return null");
 
-  t.end()
-})
+  t.end();
+});
 
-test('create an article and then update it', (t) => {
-  t.plan(4)
+test("create an article and then update it", t => {
+  t.plan(4);
 
   const create_input = {
     title: "1 Article Title",
     abst: "1 abstract text",
     body: "1 body of article"
-  }
+  };
 
-  const create_response = alice.call("articles", "main", "create_article", create_input)
+  const create_response = alice.call(
+    "articles",
+    "main",
+    "create_article",
+    create_input
+  );
 
   // console.log(create_response);
 
-  t.ok(create_response.Ok, "Alice should create an article")
-  t.deepEqual(create_response.Ok.length, 46, "Should get back article address of 46 chars")
+  t.ok(create_response.Ok, "Alice should create an article");
+  t.deepEqual(
+    create_response.Ok.length,
+    46,
+    "Should get back article address of 46 chars"
+  );
 
   const update_input = {
     article_addr: create_response.Ok,
     title: "2 Article Title",
     abst: "2 abstract text",
     body: "2 body of article"
-  }
+  };
 
-  const update_response = alice.call("articles", "main", "update_article", update_input)
+  const update_response = alice.call(
+    "articles",
+    "main",
+    "update_article",
+    update_input
+  );
 
   // console.log(update_response);
 
-  t.ok(update_response.Ok, "hdk::update_entry shouldn't return Err")
-  t.deepEqual(update_response.Ok.length, 46, "Should return address hash with 46 chars")
+  t.ok(update_response.Ok, "hdk::update_entry shouldn't return Err");
+  t.deepEqual(
+    update_response.Ok.length,
+    46,
+    "Should return address hash with 46 chars"
+  );
 
-  t.end()
-})
+  t.end();
+});
 
-test('alice create article, cameron get it', (t) => {
-  t.plan(3)
+test("alice create article, cameron get it", t => {
+  t.plan(3);
 
   const create_input = {
     title: "3 Article Title",
     abst: "3 abstract text",
     body: "3 body of article"
-  }
+  };
 
-  const create_response = alice.call("articles", "main", "create_article", create_input)
+  const create_response = alice.call(
+    "articles",
+    "main",
+    "create_article",
+    create_input
+  );
 
   // console.log(create_response);
 
-  t.ok(create_response.Ok, "Alice should create an article")
+  t.ok(create_response.Ok, "Alice should create an article");
 
   const get_input = {
     article_addr: create_response.Ok
-  }
+  };
 
-  const get_response = cameron.call("articles", "main", "get_article", get_input)
+  const get_response = cameron.call(
+    "articles",
+    "main",
+    "get_article",
+    get_input
+  );
 
   // console.log(get_response);
 
-  t.ok(get_response.Ok, "Cameron should be able to get Alice's article")
-  t.deepEqual(JSON.parse(get_response.Ok.App[1]), create_input, "Returned article should match article Alice just made in this test")
+  t.ok(get_response.Ok, "Cameron should be able to get Alice's article");
+  t.deepEqual(
+    JSON.parse(get_response.Ok.App[1]),
+    create_input,
+    "Returned article should match article Alice just made in this test"
+  );
 
-  t.end()
-})
+  t.end();
+});
 
-test('create article exceeding valid title length', (t) => {
+test("create article exceeding valid title length", t => {
   const input = {
-    title: "haH"+"A".repeat(300),
+    title: "haH" + "A".repeat(300),
     abst: "abstract text",
     body: "body of article"
-  }
+  };
 
-  const response = cameron.call("articles", "main", "create_article", input)
+  const response = cameron.call("articles", "main", "create_article", input);
 
   // console.log(response);
 
-  if (response.hasOwnProperty('Err')) {
-    if (response.Err.hasOwnProperty('Internal')) {
-      const error = JSON.parse(response.Err.Internal).kind
+  if (response.hasOwnProperty("Err")) {
+    if (response.Err.hasOwnProperty("Internal")) {
+      const error = JSON.parse(response.Err.Internal).kind;
 
       // console.log(error);
     }
   }
 
-  t.ok(JSON.parse(response.Err.Internal).kind.ValidationFailed, "Validation should fail")
+  t.ok(
+    JSON.parse(response.Err.Internal).kind.ValidationFailed,
+    "Validation should fail"
+  );
 
-  t.end()
-})
+  t.end();
+});
 
-test('alice create article, cameron fail to delete it', (t) => {
+test("alice create article, cameron fail to delete it", t => {
   const create_input = {
     title: "4 Article Title",
     abst: "4 abstract text",
     body: "4 body of article"
-  }
+  };
 
-  const create_response = alice.call("articles", "main", "create_article", create_input)
+  const create_response = alice.call(
+    "articles",
+    "main",
+    "create_article",
+    create_input
+  );
 
- console.log(create_response)
+  console.log(create_response);
 
-  t.ok(create_response.Ok, "Alice should create an article")
+  t.ok(create_response.Ok, "Alice should create an article");
 
   const delete_input = {
     article_addr: create_response.Ok
+  };
+
+  const delete_response = cameron.call(
+    "articles",
+    "main",
+    "delete_article",
+    delete_input
+  );
+
+  console.log(delete_response);
+
+  if (delete_response.hasOwnProperty("Err")) {
+    if (delete_response.Err.hasOwnProperty("Internal")) {
+      const error = JSON.parse(delete_response.Err.Internal).kind;
+
+      console.log(error);
+    }
+  } else if (delete_response.hasOwnProperty("Ok")) {
+    t.notOk(
+      delete_response.Ok === null,
+      "Cameron shouldn't be able to delete Alice's article"
+    );
   }
 
-  const delete_response = cameron.call("articles", "main", "delete_article", delete_input)
+  t.end();
+});
 
- console.log(delete_response);
-
- if (delete_response.hasOwnProperty('Err')) {
-   if (delete_response.Err.hasOwnProperty('Internal')) {
-     const error = JSON.parse(delete_response.Err.Internal).kind
-
-     console.log(error);
-   }
- } else if (delete_response.hasOwnProperty('Ok')) {
-   t.notOk(delete_response.Ok === null, "Cameron shouldn't be able to delete Alice's article")
- }
-
- t.end()
-})
-
-// container.stop()
+*/
--- ui/src/app.js
+++ ui/src/app.js
@@ -1,86 +1,81 @@
 // Create element references.
-const title = document.querySelector('#title')
-const abstract = document.querySelector('#abstract')
-const body = document.querySelector('#body')
-const submitBtn = document.querySelector('#submitBtn')
-const message = document.getElementById("message")
-const clearBtn = document.querySelector('#clearBtn')
-const address = document.querySelector('#address')
-const retrieveBtn = document.querySelector("#retrieveBtn")
-const show_article = document.querySelector('#show_article')
+const title = document.querySelector("#title");
+const abstract = document.querySelector("#abstract");
+const body = document.querySelector("#body");
+const submitBtn = document.querySelector("#submitBtn");
+const message = document.getElementById("message");
+const clearBtn = document.querySelector("#clearBtn");
+const address = document.querySelector("#address");
+const retrieveBtn = document.querySelector("#retrieveBtn");
+const show_article = document.querySelector("#show_article");
 
-const port = 8888
-const holochainUrl =  `ws://localhost:${port}/`
+const port = 8888;
+const holochainUrl = `ws://localhost:${port}/`;
 
-clearBtn.addEventListener('click', e => {
-  title.value = ''
-  abstract.value = ''
-  body.value = ''
-})
+clearBtn.addEventListener("click", e => {
+  title.value = "";
+  abstract.value = "";
+  body.value = "";
+});
 
-window.holochainClient.connect(holochainUrl)
-.then(({call, close}) => {
+window.holochainClient.connect(holochainUrl).then(({ call, close }) => {
+  submitBtn.addEventListener("click", e => {
+    e.preventDefault();
 
-  submitBtn.addEventListener('click', e => {
-    e.preventDefault()
+    submitBtn.classList.add("loading");
 
-    submitBtn.classList.add("loading")
-
-    call('info/instances')().then(info => {
+    call("info/instances")().then(info => {
       // console.log(info)
-      const instance = 'test-instance'
-      const zomeName = 'articles'
-      const functionName = 'create_article'
+      const instance = "test-instance";
+      const zomeName = "articles";
+      const functionName = "create_article";
       const params = {
         title: title.value,
         abst: abstract.value,
         body: body.value
-      }
-      const createArticle = call(instance, zomeName, functionName)
+      };
+      const createArticle = call(instance, zomeName, functionName);
 
-      createArticle(params)
-      .then((res) => {
-        const response = JSON.parse(res)
+      createArticle(params).then(res => {
+        const response = JSON.parse(res);
 
         if (response.Err) {
           console.error(response.Err);
-          submitBtn.classList.remove("loading")
-          message.style.display = "none"
+          submitBtn.classList.remove("loading");
+          message.style.display = "none";
         } else {
-          message.innerText = 'Address: ' + response.Ok
-          message.style.display = "block"
-          submitBtn.classList.remove("loading")
+          message.innerText = "Address: " + response.Ok;
+          message.style.display = "block";
+          submitBtn.classList.remove("loading");
         }
-      })
-    })
-  })
+      });
+    });
+  });
 
+  retrieveBtn.addEventListener("click", e => {
+    e.preventDefault();
 
-  retrieveBtn.addEventListener('click', e => {
-    e.preventDefault()
+    retrieveBtn.classList.add("loading");
+    show_article.style.display = "block";
 
-    retrieveBtn.classList.add("loading")
-    show_article.style.display = "block"
-
-    call('info/instances')().then(info => {
-      const instance = 'test-instance'
-      const zomeName = 'articles'
-      const functionName = 'get_article'
+    call("info/instances")().then(info => {
+      const instance = "test-instance";
+      const zomeName = "articles";
+      const functionName = "get_article";
       const params = {
         article_addr: address.value
-      }
-      const getArticle = call(instance, zomeName, functionName)
+      };
+      const getArticle = call(instance, zomeName, functionName);
 
-      getArticle(params)
-      .then((res) => {
-        const response = JSON.parse(res)
+      getArticle(params).then(res => {
+        const response = JSON.parse(res);
 
         if (response.Err) {
-          console.error(response.Err)
-          show_article.style.display = "none"
-          retrieveBtn.classList.remove("loading")
+          console.error(response.Err);
+          show_article.style.display = "none";
+          retrieveBtn.classList.remove("loading");
         } else {
-          const article = JSON.parse(response.Ok.App[1])
+          const article = JSON.parse(response.Ok.App[1]);
 
           show_article.innerHTML = `
           <div class="ui center aligned header">${article.title}</div>
@@ -88,11 +83,11 @@ window.holochainClient.connect(holochainUrl)
           <div class="ui basic segment">
             <div style="white-space: pre-wrap">${article.body}</div>
           </div>
-          `
+          `;
 
-          retrieveBtn.classList.remove("loading")
+          retrieveBtn.classList.remove("loading");
         }
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
